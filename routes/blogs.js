5e8f5ae6106aa085119a3ae7acae6d74d2aa972d
Juan <juan.giana8589@gmail.com>
2023-05-23 13:46:47 -0500

    added post rout for new-blogs

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,44 +1,45 @@
+const { Variable } = require("eslint-scope");
 const express = require("express");
 const router = express.Router();
 
 const listBlogs = [
   {
-    id: '1',
-    title: "First Blog",
-    description: "Description of the first blog",
-    author: "John Doe",
+    id: 'blog1',
+    title: 'Introduction to JavaScript',
+    description: 'Learn the basics of JavaScript programming language.',
+    author: 'Michael Johnson',
     createdAt: '2023-05-22T19:16:00.821Z',
     lastModified: '2023-05-22T19:16:00.821Z'
   },
   {
-    id: '2',
-    title: "Second Blog",
-    description: "Description of the second blog",
-    author: "Jane Smith",
+    id: 'blog2',
+    title: 'Mastering React Framework',
+    description: 'Become proficient in building web applications using React.',
+    author: 'Jane Smith',
     createdAt: '2023-05-22T19:16:00.821Z',
     lastModified: '2023-05-22T19:16:00.821Z'
   },
   {
-    id: '3',
-    title: "Third Blog",
-    description: "Description of the third blog",
-    author: "Alex Johnson",
+    id: 'blog3',
+    title: 'Deep Dive into Node.js',
+    description: 'Explore the advanced concepts of Node.js and server-side development.',
+    author: 'Michael Johnson',
     createdAt: '2023-05-22T19:16:00.821Z',
     lastModified: '2023-05-22T19:16:00.821Z'
   },
   {
-    id: '4',
-    title: "Fourth Blog",
-    description: "Description of the fourth blog",
-    author: "Emily Davis",
+    id: 'blog4',
+    title: 'CSS Tricks for Web Designers',
+    description: 'Discover useful CSS techniques to enhance your web designs.',
+    author: 'Emily Davis',
     createdAt: '2023-05-22T19:16:00.821Z',
     lastModified: '2023-05-22T19:16:00.821Z'
   },
   {
-    id: '5',
-    title: "Fifth Blog",
-    description: "Description of the fifth blog",
-    author: "Michael Brown",
+    id: 'blog5',
+    title: 'Effective Database Management',
+    description: 'Learn best practices for managing databases and optimizing performance.',
+    author: 'Robert Johnson',
     createdAt: '2023-05-22T19:16:00.821Z',
     lastModified: '2023-05-22T19:16:00.821Z'
   }
@@ -88,9 +89,49 @@ router.get("/get-blog/:id", (req, res) => {
 res.status(200).json({success: true, data: blogInfo})
 })
 
+// ◀︎ get some blogs route by author ▶︎ ///////////////////////////////////
+router.get("/get-blog-by-author/:author/", (req, res) => {
+  const author = req.params.author.toLowerCase();// grab author
+  const findIndex = listBlogs.findIndex((blog) => blog.author.toLowerCase() === author); // grab index 
+
+  const blogInfo = []; // place holder for author blog or multiple blogs
+
+  if (findIndex === -1){// check to see if author exist
+    res.status(400).json({success: false, message: "author not found"})
+  }
+  for(const blog of listBlogs){ // loop grabs blogs that correspond to author and pusses them to blogInfo Array
+    if(blog.author.toLowerCase() === author){
+      blogInfo.push(blog)
+    }
+  }
+ res.status(200).json( {success: true, data: blogInfo})
+})
+
+// ◀︎ post one blog route ▶︎///////////////////////////////////////////////
+router.post("/new-blog", (req, res) => {
+  const newBlog = { // create a new object with request body input
+    id: req.body.id,
+    title: req.body.title,
+    description: req.body.description,
+    author: req.body.author,
+    createdAt: req.body.createdAt,
+    lastModified: req.body.lastModified
+  }
+  let errorArray = []; //Variable to hold errors
+
+//checks to make sure we're not saving something that is empty or undefined
+for (let key in newBlog) { // main keys 
+  if (newBlog[key] === "" || newBlog[key] === undefined) {
+    errorArray.push(`${key} cannot be empty`);
+  }
+}
+ if (errorArray.length > 0){
+  return res.status(200).json({error: true, message: errorArray})
+ } else {
+  listBlogs.push(newBlog);
+ }
+ res.status(200).json({success: true, message: "new blog was successfully posted"});
+})
 
 module.exports = router
 
-//get one blog route by id
-//get some blogs route by author
-//post one blog route
